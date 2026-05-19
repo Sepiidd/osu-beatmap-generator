820552f6f3ab3a450a83b728d140703a3aa9ba5b
Sebastian Pedruco Choi <sebastianpedrucochoi@hotmail.com>
2026-05-19 19:16:34 -0400

    tokenizer

---
+++ src/tokens/tokenizer.py
@@ -0,0 +1,134 @@
+import json
+
+class obg_tokenizer:
+	def __init__(
+	self, 
+	load_path="", 
+	special_start=["<CIRCLE>", "<SLIDER_HEAD_BEZIER>", "<SLIDER_HEAD_LINEAR>", "<SLIDER_HEAD_PERFECT>"], 
+	special_types=["<ANCHOR>", "<SLIDER_TAIL>, <SLIDER_REPEAT>"], 
+	x_max=512, 
+	y_max=384):
+		if load_path:
+			self.load_tokens(load_path)
+			return 
+
+		self.special = special_start
+		self.special.extend(special_types)
+		self.special.extend(["<BOS>", "<EOS>", "<PAD>"])
+
+		self.special_start = special_start #tokens which indicate a new hitobject/type
+		self.special_types = special_types #other special tokens
+
+		#include max value in positions
+		self.x_positions = x_max + 1
+		self.y_positions = y_max + 1
+
+		self.itot={}
+		self.ttoi={}
+
+		index = 0
+		for s in self.special:
+			self.itot[index] = s
+			self.ttoi[s] = index
+			index+=1
+		for i in range(self.x_positions):
+			self.itot[index] = f"X_{i}"
+			self.ttoi[f"X_{i}"] = index
+			index+=1
+		for i in range(self.y_positions):
+			self.itot[index] = f"Y_{i}"
+			self.ttoi[f"Y_{i}"] = index
+			index+=1
+
+		self.num_t = index+1
+
+	#----------save+load tokens json----------
+	def save_tokens(self):
+		with open("itot.json", "w") as f:
+			json.dump(self.itot, f)
+		with open("ttoi.json", "w") as f:
+			json.dump(self.ttoi, f)
+		with open("meta.json", "w") as f:
+			json.dump({
+				"special_start": self.special_start, 
+				"special_types": self.special_types, 
+				"special": self.special, 
+				"x_positions": self.x_positions, 
+				"y_positions": self.y_positions, 
+				"num_t": self.num_t}, 
+				f
+			)
+
+	def load_tokens(load_path):
+		with open("itot.json", "r") as f:
+			self.itot = json.load(f)
+		with open("ttoi.json", "r") as f:
+			self.ttoi = json.load(f)
+		with open("meta.json", "r") as f:
+			meta = json.load(f)
+			self.special = meta[special]
+			self.special_start = meta[special_start]
+			self.special_types = meta[special_types]
+			self.x_positions = meta[x_positions]
+			self.y_positions = meta[y_positions]
+			self.num_t = meta[num_t]
+
+	#----------encode+decode----------
+	def encode_obj(self, obj):
+		return self.ttoi[obj]
+	
+	def encode_seq(self, seq):
+		seq_ids = []
+		for obj in seq:
+			seq_enc.append(encode_obj(obj))
+		return seq_ids
+
+	def decode_id(self, idx):
+		return self.itot[idx]
+
+	def decode_seq(self, seq):
+		seq_tok = []
+		for idx in seq:
+			seq_tok.append(decode_id(idx))
+		return seq_tok
+
+	#----------hitobject conversion from token----------
+	def tok_to_hitobject(self, obj_arr):
+		#TODO
+		tok = ""
+		match obj_arr[0]:
+			case "<CIRCLE>":
+				pass
+			case "<SLIDER_HEAD_BEZIER>":
+				pass
+			case "<SLIDER_HEAD_LINEAR>":
+				pass
+			case "<SLIDER_HEAD_PERFECT>":
+				pass
+		return tok
+	
+	def tok_seq_to_hitobject(self, seq):
+		seq_tok = []
+		idx = 0
+		curr = []
+		while idx < seq:
+			if len(curr) == 0: #determine current object type (circle, slider-b/l/p)
+				curr.append(seq[idx])
+			elif seq[idx] not in self.special_start:
+				curr.append(seq[idx])
+			else:
+				tok = decode_obj(curr)
+				seq_tok.append(tok)
+				curr=[seq[idx]]
+		return seq_tok
+
+if __name__ == "__main__":
+	tokenizer = obg_tokenizer()
+	print("num_t is:", tokenizer.num_t)
+	print("special is:", tokenizer.special)
+	#print("x_positions is:", tokenizer.x_positions)
+	#print("y_positions is:", tokenizer.y_positions)
+	#print("itot is:", tokenizer.itot)
+	#print("ttoi is:", tokenizer.ttoi)
+	tokenizer.save_tokens()
+
