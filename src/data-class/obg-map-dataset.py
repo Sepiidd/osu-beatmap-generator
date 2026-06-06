a51ad3aefa21829abe6cf93ab8d7876d1c94e0d4
Sebastian Pedruco Choi <sebastianpedrucochoi@hotmail.com>
2026-06-06 18:20:32 -0400

    osu map dataset class

--- src/data-class/obg-map-dataset.py
+++ src/data-class/obg-map-dataset.py
@@ -10,10 +10,43 @@ class OBGMapDataset(Dataset):
         return self.num_samples
 
     def __getitem__(self, idx):
-        with h5py.File(self.h5path, 'r') as f:
-            #TODO
-            pass
+        return self.sample_bin_search(idx)
 
-    def get_len(path):
+    def sample_bin_search(self, target):
+        with h5py.File(self.h5path, 'r') as f:
+            song_lst = list(f.keys())
+            p1 = 0
+            p2 = len(song_lst)-1
+            curr_name = None
+            sample = {}
+            found = False
+            while p1 <= p2 and not found:
+                curr = (p1+p2)//2
+                curr_name = song_lst[curr]
+                grp_set = f.get(curr_name)
+                start_range = grp_set.attrs.get("start_range")
+                end_range = grp_set.attrs.get("end_range")
+                if start_range <= target <= end_range:
+                    grp = grp_set.get(str(target))
+                    audio_feat = grp_set.get("audio_feat")[:]
+                    audio_targets = grp.get("audio_targets")[:]
+                    osu_tokens = grp.get("osu_tokens")[:]
+                    deltas_fwd = grp.get("deltas_fwd")[:]
+                    deltas_back = grp.get("deltas_back")[:]
+                    sample = {
+                            "audio_feat": audio_feat,
+                            "audio_targets": audio_targets,
+                            "osu_tokens": osu_tokens,
+                            "deltas_fwd": deltas_fwd,
+                            "deltas_back": deltas_back
+                            }
+                    found = True
+                elif target < start_range:
+                    p2 = curr-1
+                elif target > end_range:
+                    p1 = curr+1
+            return (sample, found)
+
+    def get_len(self, path):
         with h5py.File(path, 'r') as f:
             return f.attrs.get("num_samples")
