training: bug fix in metric calculation, dataclass: return entire map with benchmark flag, preprocess: apply normalization across each frequency band, not globally

src/dataclass/obg_audio_dataset.py
@@ -15,13 +15,16 @@ SEQUENCE_LEN = configA.sequence_len #equal to roughly 5 seconds of audio
 HOP_LEN = configA.hop_len
 
 class OBGAudioDataset(Dataset):
-    def __init__(self, h5path, max_seq_len, augment=False, augmentations=[]):
+    def __init__(self, h5path, max_seq_len, augment=False, augmentations=[], benchmark=False):
         self.h5path = h5path
         self.num_samples = self.get_len(h5path)
         self.max_seq_len = max_seq_len
         self.augment = augment #augmentation bool flag
         self.augmentations = augmentations.copy() #list of functions, for which each takes in inputs and targets, returns augmented inputs and targets
         self.augmentations.append(lambda x, y: (x, y))
+        self.benchmark = benchmark
+
+        self.song_num_frames = None #update per song
 
     def __len__(self):
         return self.num_samples
@@ -31,22 +34,26 @@ class OBGAudioDataset(Dataset):
         audio_feat = sample["audio_feat"]
         audio_targets = sample["audio_targets"]
 
-        #TODO: perform and retrieve augmentation, debug
         if self.augment:
             option_select = np.random.randint(0, len(self.augmentations))
             audio_feat, audio_targets = self.augmentations[option_select](audio_feat, audio_targets)
 
         num_frames = audio_feat.shape[1]
+        self.song_num_frames = num_frames
 
         if num_frames < self.max_seq_len: #edgecase, audio less than max_seq_len
             audio_feat = self.pad_feats(audio_feat)
             num_frames = audio_feat.shape[1]
 
-        start_idx = np.random.randint(0, num_frames-self.max_seq_len+1) 
+        start_idx = np.random.randint(0, num_frames-self.max_seq_len+1) if not self.benchmark else 0
+        max_len = self.max_seq_len if not self.benchmark else num_frames
 
-        window_seq = self.slice_windows(audio_feat, start_idx)
+        window_seq = self.slice_windows(audio_feat, start_idx, max_len)
         window_seq = np.swapaxes(window_seq, 1, 2) #swap axes bc im dumb
         targets = self.gen_targets(start_idx, audio_targets)
+
+        if self.benchmark:
+            return torch.tensor(audio_feat), torch.tensor(targets)
 
         return torch.tensor(window_seq), torch.tensor(targets)
 
@@ -59,7 +66,8 @@ class OBGAudioDataset(Dataset):
         time_ms = frames_to_time(start_idx, sr=SR, hop_length=HOP_LEN)*1000
         targets_idx = self.bin_search_closest(time_ms, audio_targets, lenience) 
         curr_target = audio_targets[targets_idx]
-        for i in range(self.max_seq_len):
+        max_seq_len = self.max_seq_len if not self.benchmark else self.song_num_frames
+        for i in range(max_seq_len):
             time_ms = frames_to_time(start_idx+i, sr=SR, hop_length=HOP_LEN)*1000
             t = 0
             if abs(curr_target-time_ms) < lenience:
@@ -69,13 +77,13 @@ class OBGAudioDataset(Dataset):
             targets.append(t)
         return np.array(targets)
 
-    def slice_windows(self, audio_feat, start_idx):
+    def slice_windows(self, audio_feat, start_idx, max_len):
         '''
         retrieve <self.max_seq_len> many 15x80x3 (centered input frame+context, mel frequency, window length) slices of <audio_feat>, starting from <start_idx>
         '''
         slices = []
         i = 0
-        while i < self.max_seq_len:
+        while i < max_len:
             slices.append(get_frames_at_idx(audio_feat, start_idx+i))
             i += 1
         stacked = np.stack(slices, axis=0)

src/generate/onset_generator_class.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+from librosa import load
 from configs.audio_config import AudioConfig
 from configs.training_config import TrainingConfig
 from configs.gen_config import GenConfig 
@@ -59,11 +60,19 @@ class OnsetGenerator():
             batch_seq = np.reshape(batch_seq, (self.batch_size, self.sequence_len, 15, configA.n_mel, len(configA.n_fft)))
         return batch_seq
 
-    def song_to_onsets(self, path):
+    def path_to_onsets(self, path):
         '''
         convert entire song found at <path> into a list of onset probabilities
         '''
-        features = process_audio(path) #entire song -> filtered spectrogram
+        audio, sr = load(path=path, sr=configA.sr)
+        features = process_audio(audio, sr)
+        return self.song_to_onsets(features)
+
+    def song_to_onsets(self, features):
+        '''
+        convert features spectrogram to list of onset probabilities
+        NOTE: expects input in form of (m, t, w) as describedc directly below
+        '''
         m, t, w = features.shape #shape of: mel bins, time (frame idx), window length
 
         num_predictions = torch.zeros(t).to(device)
@@ -75,7 +84,7 @@ class OnsetGenerator():
         #loop over features and track index:
         while idx < t:
             #batch, splice, index features
-            batch_seq = self.make_batch_sequence(features, idx, idx+len_batch)
+            batch_seq = self.make_batch_sequence(features, idx, idx+len_batch) #reshaped to (t, m, w)
 
             #perform model predictions
             batch_predictions = self.batch_to_onsets(batch_seq)
@@ -98,8 +107,8 @@ class OnsetGenerator():
         predictions = predictions / num_predictions 
 
         #plot for testing
-        print("shape of predictions before hamming", predictions.shape)
-        self.plot_thresholds(predictions, "plot_test")
+#        print("shape of predictions before hamming", predictions.shape)
+#        self.plot_thresholds(predictions, "plot_test")
 
         #apply hamming window across batch
         ham_window = torch.hamming_window(self.hamming_window_len, periodic=False).to(device)
@@ -107,12 +116,13 @@ class OnsetGenerator():
         #padding to maintain <output_len>=<input_len>
         #normalize hamming window to sum to one, keeps output  
         smoothed = F.conv1d(predictions.view(1, 1, -1), ham_window.view(1, 1, -1) / ham_window.sum(), padding=self.hamming_window_len//2) 
+        self.plot_thresholds(smoothed.squeeze(), "plot_test_smoothed")
 
         #convert positive prediction indices into timestamps
         predictions_bool = (smoothed > self.prediction_threshold).squeeze() #remove extra 1 dimensions along with boolean filter
-        print("predictions bool shape is", predictions_bool.shape)
+#        print("predictions bool shape is", predictions_bool.shape)
         predictions_idx = torch.nonzero(predictions_bool, as_tuple=True)[0] 
-        print("predictions idx are", predictions_idx)
+#        print("predictions idx are", predictions_idx)
         
         times = predictions_idx * configA.hop_len / configA.sr #calculation described by <https://librosa.org/doc/latest/generated/librosa.frames_to_time.html>
         return times, predictions

src/preprocess/preprocess.py
@@ -62,7 +62,7 @@ def process_audio(audio, sr):
         mel_filter = mel(sr=SR, n_fft=n, n_mels=N_MEL)
         filtered = mel_filter @ transformed
         log_scaled = power_to_db(filtered, ref=np.max)
-        normalized = (log_scaled - np.mean(log_scaled)) / np.std(log_scaled)
+        normalized = (log_scaled - np.mean(log_scaled, axis=1, keepdims=True)) / np.std(log_scaled, axis=1, keepdims=True)
 
         features.append(normalized)
     features = np.stack(features, axis=-1)